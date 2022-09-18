35632f1945b5df1397838e112bb8207997f8f80f
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-09-19 00:25:19 +0300

    update

--- test.java
+++ test.java
@@ -1,8 +1,7 @@
 public class test {// объявляется класс Test
  
     public static void main(String[] args) {// так включаются программы,
-                                           // просто запомнить
-  
+                                            // просто запомнить
        System.out.println("Hello world");
        System.out.println("5 раз");
     }
